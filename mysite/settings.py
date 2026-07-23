d5cf7458f1c9d6bd0f734d28d05f651330b16600
SakuraSport <sakurasport3@gmail.com>
2026-07-23 13:18:32 +0600

    Update ALLOWED_HOSTS for local and production

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-)yle7qsq)tsb!^s=uy1+yhs#*6j*o2ugwm*27dz^kpu5bbi25p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost",
+    "127.0.0.1",
+    "recepto-71p5.vercel.app"
+    ".vercel.app"
+]
 
 
 # Application definition
