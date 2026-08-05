318e21c00bcecd234021aa840f4fe4236704a437
SakuraSport <sakurasport3@gmail.com>
2026-08-05 12:11:36 +0600

    Обновлён доступные хосты

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-)yle7qsq)tsb!^s=uy1+yhs#*6j*o2ugwm*27dz^kpu5bbi25p
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['recepto-phi.vercel.app', 'localhost', '127.0.0.1']
 
 
 
